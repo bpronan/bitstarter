4051fd478bb2b6bbb5f47d860ec78aa06fbac61a
Brian Ronan <bpronan@gmail.com>
2013-07-05 23:39:49 +0000

    Responding from index.html

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fileBuffer = fs.readFileSync('./index.html');
-  response.send(fileBuffer.toString("utf8");
+  response.send(fileBuffer.toString("utf8"));
 });
 
 var port = process.env.PORT || 5000;
