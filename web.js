9bb0992b8d399e465f2acfe6934795f1430bea86
Brian Ronan <bpronan@gmail.com>
2013-07-05 23:31:58 +0000

    Responding from index.html

--- web.js
+++ web.js
@@ -4,13 +4,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('./index.html', function read(err, data) {
+  fs.readFileSync('./index.html', 'utf8', function read(err, data) {
     if(err) {
       throw err;
     }
-    content = data;
 
-    response.send(data.toString('utf8'));
+    response.send(data);
   });
    
 });
